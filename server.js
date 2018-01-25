6e39a0944d86a7d1a18d15da910750b92b8cb506
EDA <kimmirindel97@gmail.com>
2018-01-26 10:32:37 +1300

    created home route

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const path = require('path')
-
+// const path = require('path')
 const express = require('express')
-
 const bodyParser = require('body-parser')
-
 const hbs = require('express-handlebars')
 
 const rootRoutes = require('./routes/root')
-
 const profileRoutes = require('./routes/profile')
+const homeRoutes = require('./routes/home')
 
 const server = express()
+
+server.engine('hbs', hbs({
+  extname: 'hbs'
+}))
+
+server.set('view engine', 'hbs')
+
+server.use(bodyParser.urlencoded({extended: false}))
+server.use(express.static('public'))
+
+server.use('/', rootRoutes)
+
+server.use('/home', homeRoutes)
 
 module.exports = server
