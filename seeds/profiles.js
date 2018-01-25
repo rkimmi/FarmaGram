271c09151a67eb856bb048d5f3f0af2e353fdd80
Kimmi Rindel <kimmirindel97@gmail.com>
2018-01-26 09:29:14 +1300

    added seeds

--- seeds/profiles.js
+++ seeds/profiles.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('profiles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('profiles').insert([
+        {id: 1, bio: 'rowValue1', email: 'mike@gmail.com', user_id: 1},
+        {id: 2, bio: 'rowValue2', email: 'pepter@gmail.com', user_id: 2},
+        {id: 3, bio: 'rowValue3', email: 'judy@gmail.com', user_id: 3}
+      ])
+    })
+}
